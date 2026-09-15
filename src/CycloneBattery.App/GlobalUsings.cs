5e9bd5c3c25264757b51c36a88359e591c087640
Oleksandr Rudenko <alexrud1993@gmail.com>
2026-09-15 04:06:30 +0300

    Keep WPF aliases scoped away from tray drawing color

--- src/CycloneBattery.App/GlobalUsings.cs
+++ src/CycloneBattery.App/GlobalUsings.cs
@@ -1,6 +1,5 @@
 global using System.IO;
 global using Application = System.Windows.Application;
 global using Brush = System.Windows.Media.Brush;
-global using Color = System.Windows.Media.Color;
 global using Clipboard = System.Windows.Clipboard;
 global using MessageBox = System.Windows.MessageBox;
