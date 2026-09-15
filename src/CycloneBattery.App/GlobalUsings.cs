20f377c590735385f3ac720ff70edc1e34a0c65e
Oleksandr Rudenko <alexrud1993@gmail.com>
2026-09-15 04:04:06 +0300

    Resolve remaining WPF and IO type ambiguities

--- src/CycloneBattery.App/GlobalUsings.cs
+++ src/CycloneBattery.App/GlobalUsings.cs
@@ -1,2 +1,6 @@
+global using System.IO;
 global using Application = System.Windows.Application;
 global using Brush = System.Windows.Media.Brush;
+global using Color = System.Windows.Media.Color;
+global using Clipboard = System.Windows.Clipboard;
+global using MessageBox = System.Windows.MessageBox;
